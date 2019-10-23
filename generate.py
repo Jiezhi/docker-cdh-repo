5a2ae17ccdea491cbd555e9ccdc8930b5cb63409
Jiezhi <Jiezhi.G@gmail.com>
2019-10-23 09:55:11 +0800

    add 6.3.1

--- generate.py
+++ generate.py
@@ -6,28 +6,29 @@ import time
 from string import Template
 from os.path import join
 
-type_lst = ['cm', 'cdh']
-os_lst = ['redhat7', 'redhat6', 'sles12', 'ubuntu1604', 'ubuntu1804', 'sles12', 'generic']
+# type_lst = ['cm', 'cdh']
+os_lst = ['redhat7']
+# os_lst = ['redhat7', 'redhat6', 'sles12', 'ubuntu1604', 'ubuntu1804', 'sles12', 'generic']
 
-def generate(type, version, ops):
-    file_dir = join(ops, type)
-    if not os.path.exists(file_dir):
-        os.makedirs(file_dir)
-    file_path = join(file_dir,  'Dockerfile')
+def generate(templatefile, dockerfile_dir, version, ops):
+    if not os.path.exists(dockerfile_dir):
+        os.makedirs(dockerfile_dir)
+    file_path = join(dockerfile_dir,  'Dockerfile')
     v = version.split('.')[0]
-    with open('Dockerfile.template', 'r')as f, open(file_path, 'w') as wf:
+    with open(templatefile, 'r')as f, open(file_path, 'w') as wf:
         content = f.read()
         template = Template(content)
-        wf.write(template.substitute(type=type+v, version=version, os=ops))
+        wf.write(template.substitute(v=v, version=version, os=ops))
 
 
 def make_file(version):
     for ops in os_lst:
-        if os.path.exists(ops):
-            shutil.rmtree(ops)
-            time.sleep(5)
-        for type in type_lst:
-            generate(type, version, ops)
+        # if os.path.exists(ops):
+            # shutil.rmtree(ops)
+            # time.sleep(3)
+        generate('Dockerfile.template', ops, version, ops)
+        generate('Dockerfile-CDH.template', join(ops, 'cdh'), version, ops)
+        generate('Dockerfile-CM.template', join(ops, 'cm'), version, ops)
 
 if __name__ == '__main__':
     version = sys.argv[1]
